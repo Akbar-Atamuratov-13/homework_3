79f14a584ee84608e24fc4a265ec774a0fa61bb8
Akbar-Atamuratov-13 <157426060+Akbar-Atamuratov-13@users.noreply.github.com>
2024-01-27 00:32:05 +0100

    added file history.txt
    history 220124 group #11111

---
+++ HomeWork220124 /src/main/java/HomeWork2/HomeWork1.java
@@ -0,0 +1,37 @@
+package HomeWork2;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+        // Создать переменные всех примитивных
+        // типов и вывести их значения на экран
+
+        boolean MyBoolean = false; // (false-true)
+        System.out.println(MyBoolean);
+
+        byte MyByte = 126; // 8 bits
+        System.out.println(MyByte);
+
+        short MyShort = 29765; // 16 bits
+        System.out.println(MyShort);
+
+        int MyInt = 1443245; // 32 bits
+        System.out.println(MyInt);
+
+        long MyLong = 42565422; // 64 bits
+        System.out.println(MyLong);
+
+        float MyFloat = 43.42f; // 32 bits
+        System.out.println(MyFloat);
+
+        double MyDouble = 43.466; // 64 bits
+        System.out.println(MyDouble);
+
+        char MyChar = 'g'; // 16 bits
+        System.out.println(MyChar);
+
+
+    }
+}
---
+++ HomeWork220124 /src/main/java/HomeWork2/HomeWork5.java
@@ -0,0 +1,18 @@
+package HomeWork2;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+
+        // Дана целая переменная с трехзначным значением.
+        // Вывести на экран все цифры этого числа.
+        //Должно работать для произвольного трехзначного числа.
+        //Например, если значение равно 347, вывести на экран
+        // "первая: 3, вторая: 4, третья: 7"
+
+        String Nummer = "653";
+        System.out.println("первая : " + Nummer.charAt(0));
+        System.out.println("вторая : " + Nummer.charAt(1));
+        System.out.println("третья : " + Nummer.charAt(2));
+
+    }
+}
